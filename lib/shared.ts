78d2eb0df18cfba8e1d02f2fadc1233580eddbe5
Matej Sveda <msveda@engeto.com>
2022-05-14 19:40:01 +0200

    revert changes

--- lib/shared.ts
+++ lib/shared.ts
@@ -81,23 +81,6 @@ export interface RegisterFormState {
 	};
 }
 
-export type Volunteer = {
-	name: string;
-	email: string;
-	phone: string;
-	organization: string;
-	contactHours: string;
-	expertise: string;
-	verified: boolean;
-	banned: boolean;
-	languages: {
-		language: {
-			id: string;
-			name: string;
-		};
-	}[];
-};
-
 export interface HelpFormState {
 	name: string;
 	phone: string;
